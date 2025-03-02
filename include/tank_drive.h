ed3f4455f09f6f21e398025d691ba2212ad3d0a0
Varun Chinthoju <167709634+Varun-Chinthoju@users.noreply.github.com>
2025-03-01 19:09:24 -0800

    Made tank drive code

---
+++ include/tank_drive.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include <utility>
+
+std::pair<double, double> tank_drive(motor_group Left_motors, motor_group Right_motors);
---
+++ src/tank_drive.cpp
@@ -0,0 +1,23 @@
+#include <algorithm>
+#include <utility>
+
+#include "robot-config.h"
+
+void tank_drive(motor_group Left_motors, motor_group Right_motors, int Max_Speed) {
+    while (true) {
+        // Get the joystick values
+        int leftSpeed = Controller1.Axis3.position();
+        int rightSpeed = Controller1.Axis2.position();
+
+        // Scale the speeds to the maximum speed
+        leftSpeed = leftSpeed * Max_Speed / 100;
+        rightSpeed = rightSpeed * Max_Speed / 100;
+
+        // Set the motor speeds
+        Left_motors.spin(forward, leftSpeed, percent);
+        Right_motors.spin(forward, rightSpeed, percent);
+
+        // Allow other tasks to run
+        task::sleep(20);
+    }
+}

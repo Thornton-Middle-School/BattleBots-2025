f2dc60acfb8de070274e08d01c9643fccf254454
Varun Chinthoju <varchi617@fusdk12.net>
2025-03-11 13:31:24 -0700

    Add missing closing brace and use namespace in tank_drive.cpp

--- src/tank_drive.cpp
+++ src/tank_drive.cpp
@@ -4,6 +4,7 @@
 #include "robot-config.h"
 #include "tank_drive.h"
 
+using namespace vex;
 void tank_drive(motor_group Left_motors, motor_group Right_motors, int Max_Speed) {
     while (true) {
         // Get the joystick values
